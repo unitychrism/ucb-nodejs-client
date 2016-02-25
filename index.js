06cbe0fb32d6c06cdece615f213633230e8ae712
Chris Mahoney <chrism@unity3d.com>
2016-02-25 01:18:25 -0600

    Updated tests

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ var Swagger = require('swagger-client');
 var apiToken = '';
 var client = function(url, token, callback) {
     console.log('Loading Unity Cloud Build schema from ' + url);
-    apiToken = token;
+
+    if (typeof(token) !== 'function') {
+        apiToken = token;
+    } else {
+        callback = token;
+    }
+
     new Swagger({
         url: url,
         usePromise: true
     })
     .then(function (client) {
         console.log('Unity Cloud Build client loaded OK.');
-        client.clientAuthorizations.add('Basic', new Swagger.PasswordAuthorization(apiToken, ''));
-        //client.authorizations.add('Basic', new auth.PasswordAuthorization(apiToken, ''));
+
+        if (apiToken) {
+            client.clientAuthorizations.add('Basic', new Swagger.PasswordAuthorization(apiToken, ''));
+        }
         callback(client);
     })
     .catch(function(error) {
--- test/index.js
+++ test/index.js
@@ -7,14 +7,15 @@ var config = {
     schemaurl: 'https://build-api.cloud.unity3d.com/api/v1/api.json'
 }
 
-describe('#client', function() {
-    var testClient;
-    before(function(done) {
-        ucbclient(config.schemaurl, function(client) {
-            testClient = client;
-            done();
-        });
+var testClient;
+before(function(done) {
+    ucbclient(config.schemaurl, function(client) {
+        testClient = client;
+        done();
     });
+});
+
+describe('#client', function() {
 
     it('is an object', function() {
         expect(testClient).to.be.a('object');
@@ -42,5 +43,16 @@ describe('#client', function() {
 
     it('has shares', function() {
         expect(testClient).to.have.property('shares');
+    });
+
+    it('fails to get self user without token', function(done) {
+        testClient.users.getUserSelf()
+            .then(function(user) {
+
+            })
+            .catch(function(error) {
+                expect(error.status).to.equal(401);
+                done();
+            });
     });
 });

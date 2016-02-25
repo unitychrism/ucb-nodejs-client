5f8ce6273166cba04a6ca8f6f1801601b65d6a6a
Chris Mahoney <chrism@unity3d.com>
2016-02-24 23:02:52 -0600

    Moved implementation outside of package

--- app.js
+++
@@ -1,8 +0,0 @@
-var ucb = require('./index'),
-    config = require('./config');
-
-ucb.client(config.schemaurl, function(client) {
-    for (var property in client.apis) {
-        console.log('property: ' + property);
-    }
-});
--- config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    schemaurl: 'https://build-api.cloud.unity3d.com/api/v1/api.json'
-};

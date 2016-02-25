f829574ece6c39587879aeb36f10e84fde44b99f
Chris Mahoney <chrism@unity3d.com>
2016-02-25 00:13:06 -0600

    Added token authentication step

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 var Swagger = require('swagger-client');
 
-exports.client = function(url, token, callback) {
+var apiToken = '';
+var client = function(url, token, callback) {
     console.log('Loading Unity Cloud Build schema from ' + url);
+    apiToken = token;
     new Swagger({
         url: url,
-        authorizations: {
-            easyapi_basic: new client.PasswordAuthorization(token, ''),
-        },
         usePromise: true
     })
     .then(function (client) {
         console.log('Unity Cloud Build client loaded OK.');
+        client.clientAuthorizations.add('Basic', new Swagger.PasswordAuthorization(apiToken, ''));
+        //client.authorizations.add('Basic', new auth.PasswordAuthorization(apiToken, ''));
         callback(client);
     })
     .catch(function(error) {
         console.log('UCB error: ' + error);
     });
 };
+exports.client = client;
